a0bc0b54219be7d57653335e7b254309f7c18847
anusha <ellaboinabikshapathi@gmail.com>
2026-05-14 11:56:13 +0530

    Mark hello.c as day 1 of C

--- hello.c
+++ hello.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+// Day 1 - 100 Days of C Challen
+#include <stdio.h>#include <stdio.h>
 int main() {
     printf("Hello World - Day 1 C\n");
     printf("100 Days of Code: Anusha 2026\n");
