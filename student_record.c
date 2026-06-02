6aacf581378e264eec33ef225f728ca98bec04ae
anusha <ellaboinabikshapathi@gmail.com>
2026-06-02 17:13:16 +0530

    Create student_record.c

---
+++ student_record.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+struct Student {
+    int rollNo;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    struct Student s[3];
+    int i;
+
+    printf("Enter details of 3 students:\n");
+
+    for(i = 0; i < 3; i++) {
+        printf("\nStudent %d\n", i + 1);
+
+        printf("Roll No: ");
+        scanf("%d", &s[i].rollNo);
+
+        printf("Name: ");
+        scanf("%s", s[i].name);
+
+        printf("Marks: ");
+        scanf("%f", &s[i].marks);
+    }
+
+    printf("\n--- Student Records ---\n");
+
+    for(i = 0; i < 3; i++) {
+        printf("\nRoll No: %d", s[i].rollNo);
+        printf("\nName: %s", s[i].name);
+        printf("\nMarks: %.2f\n", s[i].marks);
+    }
+
+    return 0;
+}
