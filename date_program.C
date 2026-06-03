bb9cbd512fe4b386c8959e49d11e25cda5c24699
anusha <ellaboinabikshapathi@gmail.com>
2026-06-03 22:01:51 +0530

    Create date_program.C

---
+++ date_program.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // Day 31 Program by Anusha
+    // GitHub: github.com/your-username
+    // Blog: anusha-python.hashnode.dev
+    
+    printf("Hello Anusha! \n");
+    printf("Today is Day 31 of my coding journey \n");
+    printf("I am learning C + Python together \n");
+    printf("From Warangal to the World! \n");
+    
+    printf("\n--- Calculation --- \n");
+    int day = 31;
+    int hours_coded = day * 2; // 2 hours per day
+    
+    printf("Total days coded: %d \n", day);
+    printf("Total hours coded: %d hours \n", hours_coded);
+    printf("Keep going Anusha! \n");
+    
+    return 0;
+}
